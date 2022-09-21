b79f0ee58fac3db611fc3941bf7e3465830298d3
MasonPaddie <masonpaddie29@yahoo.com>
2022-09-21 18:21:58 -0500

    square component created

---
+++ app/src/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <p>I am a square</p>
+        );
+    }
+}
+
+  export default Square
