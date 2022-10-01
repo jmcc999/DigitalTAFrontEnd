6b7281917810147100f92e83db3965310a31ca83
MasonPaddie <masonpaddie29@yahoo.com>
2022-10-01 14:22:42 -0500

    month reads previous value

--- app/src/MonthDropdown.js
+++ app/src/MonthDropdown.js
@@ -5,26 +5,18 @@ class MonthDropdown extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            options : [
-                {
-                    name: 'January',
-                    startDay: 5,
-                    length: 31
-                },
-                {
-                    name: 'February',
-                    startDay: 2,
-                    length: 28  
-                },
-                // {
-                //     name: 'March',
-                //     value: [((this.state.options.value[1] % 7) + this.state.value[0]),'31']   
-                // },
-                // {
-                //     name: 'April',
-                //     value: [((this.state.options.value[1] % 7) + this.state.value[0]),'30']   
-                // },
-            ],
+            Jan : { 
+                name: 'January',
+                startDay: 5,
+                length: 31
+            },
+
+            Feb: {
+                name: 'February',
+                startDay: '',
+                length: 28  
+            },
+
             value: '?'
         }
         this.handleChange = this.handleChange.bind(this)
@@ -35,18 +27,22 @@ class MonthDropdown extends React.Component {
     }
 
     handleChange = (event) => {
+        this.setState({Feb: {
+            name: 'February',
+            startDay: this.state.Jan.startDay + (this.state.Jan.length % 7),
+            length: 28 
+        }})
         this.setState({value: [event.target.value]})
         this.props.callbackFoo(this.state.value[0],(this.state.value[2] + this.state.value[3]))
         console.log(this.state.value)
     }
 
     render() {
-        const {options, val} = this.state
 
         return (
-            <select onChange={(event) => this.handleChange(event)} name={val} value={val}>
-                <option value={[options[0].startDay,options[0].length]} name={options[0].name}>{options[0].name}</option>
-                <option value={[options[1].startDay,options[1].length]} name={options[1].name}>{options[1].name}</option>
+            <select onChange={(event) => this.handleChange(event)} name='monthDropdown' value={this.state.value}>
+                <option value={[this.state.Jan.startDay,this.state.Jan.length]} name={this.state.Jan.name}>{this.state.Jan.name}</option>
+                <option value={[this.state.Feb.startDay,this.state.Feb.length]} name={this.state.Feb.name}>{this.state.Feb.name}</option>
                 {/* <option value={options[2].value} name={options[2].name}>{options[2].name}</option>
                 <option value={options[3].value} name={options[3].name}>{options[3].name}</option> */}
             </select>
